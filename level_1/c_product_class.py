2d3d805fd670dad63eb980eadefe16c2b3e2cbc1
Vitaly Lunev <v.lunev.mail@gmail.com>
2023-10-02 23:20:40 +0300

    Level 1: task c

--- level_1/c_product_class.py
+++ level_1/c_product_class.py
@@ -6,11 +6,27 @@
     2. Создайте экземпляр этого продукта и наполинте своими данными.
     3. Распечатайте о нем иформацию в таком виде: Информация о продукте: название, описание, цена, вес
 """
+from decimal import Decimal
 
 
 class Product:
-    pass  # код писать тут
+    def __init__(
+        self, name: str, description: str, price: Decimal, weight: Decimal
+    ) -> None:
+        self.name = name
+        self.description = description
+        self.price = price
+        self.weight = weight
 
 
 if __name__ == '__main__':
-    pass  # код писать тут
+    product = Product(
+        name='Пончик с повидлом',
+        description='Пончик с начинкой из фруктового повидла.',
+        price=Decimal('120.00'),
+        weight=Decimal('75.00')
+    )
+    print(
+        f"Информация о продукте: {product.name}, {product.description},"
+        f" {product.price}, {product.weight}"
+    )
